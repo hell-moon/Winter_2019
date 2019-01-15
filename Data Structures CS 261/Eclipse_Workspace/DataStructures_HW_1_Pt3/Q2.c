a5d637085a3ffa09c969674713ccd8e8dae1fdd3
Unknown <kuangx@oregonstate.edu>
2019-01-15 00:53:43 -0800

    Completed majority of Q2.c, not sure if swap function is correct

--- Data Structures CS 261/Eclipse_Workspace/DataStructures_HW_1_Pt3/Q2.c
+++ Data Structures CS 261/Eclipse_Workspace/DataStructures_HW_1_Pt3/Q2.c
@@ -7,26 +7,49 @@
 #include <stdlib.h>
 #include <time.h>
 
+int randomize(int max){
+	return (rand() % max);
+}
+
+
+void swap(int* a, int* b){
+	//store address pointed to by 'a' in temp variable
+	int* temp = a;
+	//set a to point to address that 'b' points to
+	a = b;
+	b = temp;
+}
+
 int foo(int* a, int* b, int c){
     /*Swap the addresses stored in the pointer variables a and b*/
-    
+	swap(a, b);
+
     /*Decrement the value of integer variable c*/
+	c--;
     
     /*Return c*/
+	return c;
 }
 
 int main(){
+	srand(time(NULL));
+
     /*Declare three integers x,y and z and initialize them randomly to values in [0,10] */
-    
+    int x, y, z = 0;
+    x = randomize(10);
+    y = randomize(10);
+    z = randomize(10);
     /*Print the values of x, y and z*/
+    printf("Values of x, y, and z: %d %d %d\n", x, y, z);
     
     /*Call foo() appropriately, passing x,y,z as parameters*/
-    
+    int returned = foo(&x, &y, z);
     /*Print the values of x, y and z*/
+    printf("Values of x, y, and z are now: %d %d %d\n", x, y, z);
     
     /*Print the value returned by foo*/
- 
-    
+    printf("Value returned by foo: %d\n", returned);
+
     return 0;
 }
     
